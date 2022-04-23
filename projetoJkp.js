72c3bc67cd33d45e811b86123b984f3e3cd6a381
iorge <jorgez.anjos@gmail.com>
2022-04-23 12:35:49 -0300

    commit correções

--- projetoJkp.js
+++ projetoJkp.js
@@ -1,10 +1,12 @@
 const prompt = require('prompt-sync')();
+let jogar;//variavel que vai fazer o jogo rodar novamente, caso o usuario queira; preferi botar aqui fora para não usar o var.
 do {
     console.clear();//botei o console.clear aqui dentro do while, para quando o usuario escolher reiniciar, possa limpar o jogo anterior.
     let vitorias = 0 
     let derrotas = 0
     let empate = 0
     let alternativas = [0,1,2]//[0]pedra, [1]papel, [2]tesoura.
+   
     console.log("Esse é um pequeno jogo de Jokenpo, um jogo simples, em que temos 3 escolhas, sendo elas: pedra, papel e tesoura");
     console.log("Sendo que: PEDRA VENCE DE TESOURA; TESOURA VENCE DE PAPEL; PAPEL VENCE DE PEDRA.");//introdução do jogo fora do for, para não ficar maçante a repetição
     console.log();
@@ -47,11 +49,17 @@ for(let i=0;i<rodadas;i++){
         console.log(`VOCÊ VENCEU ${vitorias} VEZES, FOI DERROTADO ${derrotas} VEZES E EMPATOU ${empate} VEZES`);
         console.log("VOCÊ EMPATOU");
     }
-    var jogar = prompt("Deseja continuar?(sim/não): ");//Aqui utilixei o var devido o escopo global dela.
-  if(jogar=="sim"){
+    jogar = prompt("Deseja continuar?(s/n): ").toLowerCase();
+  while(jogar!="s" && jogar!="n"){
+    console.log()  
+    console.log('Responda apenas com "s" ou "n"')
+    jogar = prompt("Deseja continuar?(s/n): ").toLowerCase();
+  }
+    if(jogar=="s"){
       continue;
-  }else if(jogar=="não"){
-      console.log("Tudo bem, muito obrigado.");
+  }else if(jogar=="n"){
+    console.log()  
+    console.log("Tudo bem, muito obrigado.");
       break;
   }
-} while (jogar=="sim");
+} while (jogar=="s");
